568c9a2d2f30a30cedf9118ccf065ba43219138f
InJung Chung <mu29gl@gmail.com>
2015-05-31 02:56:03 +0900

    initial commit

---
+++ fate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fate
+//
+//  Created by 정인중 on 2015. 5. 31..
+//  Copyright (c) 2015년 스튜디오 작은별. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
